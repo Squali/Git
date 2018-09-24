ad8018ddd6b05b636b5210cc4d272e52a06fbbfe
posselin <pierre.osselin@student.ecp.fr>
2018-09-24 20:34:09 +0200

    Externalization userInput

---
+++ MasterMind/InputAlgo.py
@@ -0,0 +1,11 @@
+# Module Made to build utilities functions
+
+
+# Compare two configurations and give the results
+# We suppose the colors are different
+
+__all__ = ["consoleInput"]
+
+
+def consoleInput():
+    return input("Result obtained (white, red)")
--- MasterMind/Main.py
+++ MasterMind/Main.py
@@ -1,29 +1,24 @@
 from Utils import *
+from InputAlgo import *
 from random import sample
 
 # Test Module
 
 
 def main():
-    l = list(range(8))
-    lSample1 = sample(l, 4)
-    lSample2 = sample(l, 4)
-    setSample = [sample(l, 4) for i in range(20)]
-    # print setSample
-    # print createPartition(lSample1, setSample)
-    return selectConfig(setSample)
+    return mastermind(consoleInput)
 
 
-def mastermind():
+def mastermind(functionInput):
     setConfig = [[i, j, k, l] for i in range(8) for j in range(8) for k in range(8) for l in range(8) if i != j and i != k and i != l and j != k and j != l and k != l]
     print ("Configuration to play : [0,1,2,3]")
     currentConfig = [0, 1, 2, 3]
-    result = input("Result obtained (white, red)")
+    result = functionInput()
     setConfig = updateSet(currentConfig, result, setConfig)
     while (len(setConfig) > 1):
         currentConfig = selectConfig(setConfig)
         print ("Configuration to play : " + ''.join(str(e) for e in currentConfig))
-        result = input("Result obtained (white, red)")
+        result = functionInput()
         setConfig = updateSet(currentConfig, result, setConfig)
     print ("Hidden Combination :" + ''.join(str(e) for e in currentConfig))
     return currentConfig
