ca89412395d7d1589326bedb2e863911c1cad1d3
Pierre OSSELIN <pierre.osselin@spc.ox.ac.uk>
2019-01-29 11:12:07 +0100

    COde R for EM

---
+++ Dissertation/Rfiles/Sampson.R
@@ -0,0 +1,27 @@
+## Main Script
+source("/home/pierre/Git/Dissertation/Rfiles/Utils.R")
+source("/home/pierre/Git/Dissertation/Rfiles/EM_Algorithm.R")
+data("sampson")
+n <- 18
+mat <- sampson[["SAMPLK1"]]
+print(mat)
+mat["ALBERT_16", "AMAND_13"] <- 3
+mat["ALBERT_16", "ROMUL_10"] <- 0
+rho <- matrix(c(0), 18,3)
+for (i in 1:n){
+  for (j in 1:K){
+    if (mat[i,j] > 0){
+      rho[i,mat[i,j]] <- j
+    }
+    
+  }
+}
+
+shape <- dim(rho)
+n <- shape[1]
+K <- shape[2]
+p <- 3
+a <- 2
+b <- 2
+Wresult <- EMInference(rho[,1:K],p,a,b, tol = 0.000001)
+print(Wresult)
---
+++ Dissertation/Rfiles/Utils.R
@@ -0,0 +1,36 @@
+## Compute the mean difference in ranking
+rankingGap <- function(v1,v2){
+  n <- length(v1)
+  l1 <- 1:n
+  l2 <- match(v1,v2)
+  l3 <- l1 - l2
+  l3 <- abs(l3)
+  res <- mean(l3)
+  return(res)
+}
+
+## Generate list Plackett-Luce Model
+listPlackett <- function(n, K, lambda){
+  rho <- c()
+  for (i in 1:K){
+    prob <- lambda / sum(lambda)
+    res <- rmultinom(1,1, prob = prob)
+    ind <- match(c(1), res)
+    lambda[ind] <- 0
+    rho <- c(rho, ind)
+  }
+  return(rho)
+}
+
+## Generate Networks with community representation
+
+generateNetwork <- function(n,a,b,p,K){
+  W <- matrix(rgamma(n*p, a, rate = b), n, p)
+  lambda <- W %*% t(W)
+  for (i in 1:n){
+    lambda[i,i] <- 0
+  }
+  rho <- apply(lambda, 1, (function(x) listPlackett(n, K, x))) 
+  return(t(rho))
+}
+
