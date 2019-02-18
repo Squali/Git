c0766b764fe547004a5e39788e3cfdb02cc6d914
Pierre OSSELIN <pierre.osselin@spc.ox.ac.uk>
2019-02-18 11:55:30 +0100

    R updates

--- Dissertation/Rfiles/InferenceCovariates.R
+++ Dissertation/Rfiles/InferenceCovariates.R
@@ -1,6 +1,6 @@
 library("MASS", lib.loc="/usr/lib/R/library")
-library("MHadaptive", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.5")
 library("mvtnorm", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.5")
+library("invgamma")
 ## First, there will be no covariates beta = 0
 
 # s = prior in beta, a, b. r = prior in the inverse wishart
@@ -25,9 +25,9 @@ loglikelihood <- function(lambda, rho) {
   L <- sum(sum(log(Delta)) + sum(log(interm)))
 }
 data <- function(){
-  n <- 100
+  n <- 6
   priorParam <- 10**(5)
-  priorSigma <- 10
+  priorSigma <- 0.01
   priorParamWi <- 10**(5)
   lengthCov <- 0
   return(list("n" = n, "priorParam" = priorParam, "priorParamWi" = priorParamWi, "lengthCov" = lengthCov, "priorSigma" = priorSigma))
@@ -38,7 +38,6 @@ targetdistrib <- function(vecParam, rho = rho) {
   shape <- dim(rho)
   K <- shape[2]
   dat <- data()
-  n <- dat[["n"]]
   n <- shape[1]
   pr <- dat[["priorParam"]]
   prwi <- dat[["priorParamWi"]]
@@ -47,9 +46,8 @@ targetdistrib <- function(vecParam, rho = rho) {
   a <- vecParam[1:n]
   b <- vecParam[(n+1):(2*n)]
   sig <- vecParam[(2*n + 1)]
-  r <- vecParam[(2*n + 2)]
+  realr <- vecParam[(2*n + 2)]
   realSigma <- exp(sig)
-  realr <- exp(r)
   lambdavec <- matrix(vecParam[(2*n + 3):(2*n + 2 + n*(n-1))], n, (n-1), byrow = TRUE)
   interm <- matrix(0,n,n)
   for (i in 1:n){
@@ -63,8 +61,9 @@ targetdistrib <- function(vecParam, rho = rho) {
   }
   
   #Computation prio
-  prior <- -sum((a**2)/(2*(pr**2))) - sum((b**2)/(2*(pr**2))) + condBeta -10*(sig) - (5/2)*log(1-(realr)**2) - (prwi/((realSigma**2)*(1-r**2))) - r**2/(2*((prSigma)**2)) -sig**2/(2*((prSigma)**2))
-  
+  print(condBeta)
+  prior <- -sum((a**2)/(2*(pr**2))) - sum((b**2)/(2*(pr**2))) + condBeta - 1.01*sig - 1.01/realSigma
+  #-10*(realSigma) - (5/2)*log(1-(realr)**2) - (prwi/((realSigma**2)*(1-r**2))) - r**2/(2*((prSigma)**2)) -sig**2/(2*((prSigma)**2))
   print("prior")
   print(prior)
   # Computation 2nd term
@@ -93,10 +92,45 @@ targetdistrib <- function(vecParam, rho = rho) {
     }
   }
   thirdTerm <- -sum(log(Delta)) + sum(interm)
-  print("third term")
   print(thirdTerm)
-  
-  if (realr > 1) {
-    return(-Inf)
-    } else {return(prior + summation + thirdTerm)}
+  print("result target")
+  print(prior + summation + thirdTerm)
+  return(prior + summation + thirdTerm)
 }
+
+
+proposalfunction <- function(vecParam){
+  dat <- data()
+  n <- dat[["n"]]
+  pr <- dat[["priorParam"]]
+  prwi <- dat[["priorParamWi"]]
+  prSigma <- dat[["priorSigma"]]
+  lengthCov <- dat[["lengthCov"]]
+  a <- vecParam[1:n]
+  b <- vecParam[(n+1):(2*n)]
+  l1 = vecParam[1:(2*n)]
+  l = rnorm(2*n, mean = l1, sd = rep(1, 2*n))
+  l2 = rinvgamma(1, shape = prSigma, rate = prSigma)
+  l3 = runif(1)
+  return(c(l,l2,l3))
+}
+
+run_metropolis_MCMC <- function(startvalue, iterations, rho){
+  d <- length(startvalue)
+  chain = array(dim = c(iterations+1,d))
+  chain[1,] = startvalue
+  for (i in 1:iterations){
+    proposal = proposalfunction(chain[i,])
+    
+    probab = exp(targetdistrib(proposal, rho = rho) - targetdistrib(chain[i,], rho = rho))
+    print(targetdistrib(proposal, rho = rho))
+    if (runif(1) < probab){
+      chain[i+1,] = proposal
+    }else{
+      chain[i+1,] = chain[i,]
+    }
+  }
+  return(chain)
+}
+
+s
--- Dissertation/Rfiles/MainCovariates.R
+++ Dissertation/Rfiles/MainCovariates.R
@@ -1,17 +1,45 @@
 source("/home/pierre/Git/Dissertation/Rfiles/InferenceCovariates.R")
 source("/home/pierre/Git/Dissertation/Rfiles/Utils.R")
+library("mcmc")
 
 n <- 6
 K <- 3
 p <- 2
 a <- 2
 b <- 2
+nbIterations <- 2000
 rho <- generateNetwork(n,a,b,p,K)
 a <- runif(n, min = -1, max = 1)
 b <- runif(n, min = -1, max = 1)
 sig <- 0
-r <- log(0.9)
+r <- 0.5
 lambda <- rnorm(n*(n-1), mean = 0, sd = 1)
 vectParam <- c(a, b, sig, r, lambda)
 print(vectParam)
 targetdistrib(vectParam, rho = rho)
+
+
+chain <- run_metropolis_MCMC(vectParam, nbIterations, rho)
+
+burnIn = 100
+acceptance = 1-mean(duplicated(chain[-(1:burnIn),]))
+
+par(mfrow = c(2,3))
+hist(chain[-(1:burnIn),1],nclass=30, , main="Posterior of a", xlab="True value = red line" )
+abline(v = mean(chain[-(1:burnIn),1]))
+abline(v = trueA, col="red" )
+hist(chain[-(1:burnIn),2],nclass=30, main="Posterior of b", xlab="True value = red line")
+abline(v = mean(chain[-(1:burnIn),2]))
+abline(v = trueB, col="red" )
+hist(chain[-(1:burnIn),3],nclass=30, main="Posterior of sd", xlab="True value = red line")
+abline(v = mean(chain[-(1:burnIn),3]) )
+abline(v = trueSd, col="red" )
+plot(chain[-(1:burnIn),1], type = "l", xlab="True value = red line" , main = "Chain values of a", )
+abline(h = trueA, col="red" )
+plot(chain[-(1:burnIn),2], type = "l", xlab="True value = red line" , main = "Chain values of b", )
+abline(h = trueB, col="red" )
+plot(chain[-(1:burnIn),3], type = "l", xlab="True value = red line" , main = "Chain values of sd", )
+abline(h = trueSd, col="red" )
+
+# for comparison:
+summary(lm(y~x))
